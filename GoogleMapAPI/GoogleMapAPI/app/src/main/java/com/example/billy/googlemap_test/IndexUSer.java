048a270346ee0e003b1c4a28c3bf41b10588a8ef
duyuit <duykkxm92@gmail.com>
2017-08-31 11:11:57 +0700

    1
    1

--- GoogleMapAPI/GoogleMapAPI/app/src/main/java/com/example/billy/googlemap_test/IndexUSer.java
+++ GoogleMapAPI/GoogleMapAPI/app/src/main/java/com/example/billy/googlemap_test/IndexUSer.java
@@ -1,15 +1,10 @@
 package com.example.billy.googlemap_test;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import sqlite.Databasehelper;
 
 public class IndexUSer extends AppCompatActivity {
 
@@ -19,6 +14,7 @@ public class IndexUSer extends AppCompatActivity {
         setContentView(R.layout.activity_index_user);
         Button button = findViewById(R.id.button2);
         Button btnlogin = findViewById(R.id.button);
+
 
 
         //cursor.close();
