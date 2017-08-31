95279877ec07a23a59fea2e393d793ac1530f320
hoideptrai123 <toilahoi007@gmail.com>
2017-08-31 22:14:18 +0700

    change

--- GoogleMapAPI/GoogleMapAPI/app/src/main/java/sqlite/Databasehelper.java
+++ GoogleMapAPI/GoogleMapAPI/app/src/main/java/sqlite/Databasehelper.java
@@ -33,7 +33,7 @@ public class Databasehelper extends SQLiteOpenHelper {
 
     //Create a empty database on the system
     public void createDatabase() throws IOException {
-        db_delete();
+       // db_delete();
 
         boolean dbExist = checkDataBase();
 
