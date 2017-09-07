59445a34006c618b78f111c06eb5c31f6d5b37e4
hoideptrai123 <toilahoi007@gmail.com>
2017-09-07 20:03:12 +0700

    change

--- GoogleMapAPI/GoogleMapAPI/app/src/main/java/com/example/billy/googlemap_test/Index.java
+++ GoogleMapAPI/GoogleMapAPI/app/src/main/java/com/example/billy/googlemap_test/Index.java
@@ -1,5 +1,6 @@
 package com.example.billy.googlemap_test;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -11,7 +12,9 @@ import android.location.Geocoder;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -20,6 +23,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,6 +33,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
@@ -44,6 +49,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import adapter.adapterlocation;
 import model.Location;
@@ -53,21 +59,23 @@ public class Index extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap map;
     ListView listView;
     ArrayList<Location> arrayList;
-
-
+  TextView editText7;
+    double latitude,longitude;
     adapterlocation arrayAdapter;
-
+int PROXIMITY_RADIUS=1000;
     Databasehelper myDatabase = new Databasehelper(this);
     SQLiteDatabase database;
-
+     LatLng userLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Cửa hàng");
-       // ActivityCompat.requestPermissions(Index.this,new String[]{Manifest.permission.,Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        ActivityCompat.requestPermissions(Index.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
 
+        // ActivityCompat.requestPermissions(Index.this,new String[]{Manifest.permission.,Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        editText7=findViewById(R.id.editText7);
         myDatabase.Khoitai();
         database = myDatabase.getMyDatabase();
         //ready map
@@ -95,7 +103,33 @@ public class Index extends AppCompatActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         LatLng sushi1 = new LatLng(10.780407, 106.6916509);
         map = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
 
+            map.setMyLocationEnabled(true);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+
+        }
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = service.getBestProvider(criteria, false);
+        android.location.Location location = service.getLastKnownLocation(provider);
+      userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        latitude = location.getLatitude();
+//
+        longitude = location.getLongitude();
+////
+//       lastlocation = location;
+
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
 
 //        map.addMarker(new MarkerOptions()
 //                .title("YEN SUSHI & SAKE PUB 1")
@@ -128,41 +162,104 @@ public class Index extends AppCompatActivity implements OnMapReadyCallback {
             return;
         }
 
-        map.setMyLocationEnabled(true);
-        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = service.getBestProvider(criteria, false);
-        android.location.Location location = service.getLastKnownLocation(provider);
-        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
-        Toast.makeText(this, location.getLatitude()+" ", Toast.LENGTH_SHORT).show();
-        Circle circle = map.addCircle(new CircleOptions()
-                .center(userLocation)
-                .radius(1000)
-                .fillColor(0x550000FF));
+
 
     }
 
     //Add restaurant
     void AddMakerCustom()
     {
-        List<Address> addresses=null;
-        Geocoder geocoder =new Geocoder(this);
+        Geocoder geoCoder = new Geocoder(Index.this, Locale.getDefault());
+        List<Address> addresses = null;
         try {
-            addresses= geocoder.getFromLocationName("Paris",1);
-            while (addresses.size()==0) {
-                addresses = geocoder.getFromLocationName("Paris", 1);
+            addresses = geoCoder.getFromLocationName(editText7.getText() + "", 5);
+            Toast.makeText(Index.this, addresses.size() + "", Toast.LENGTH_SHORT).show();
+            if (addresses.size() > 0) {
+                Double lat = (double) (addresses.get(0).getLatitude());
+
+                Toast.makeText(Index.this, addresses.get(0).getLatitude() + "", Toast.LENGTH_SHORT).show();
+                Double lon = (double) (addresses.get(0).getLongitude());
+
+                Log.d("lat-long", "" + lat + "......." + lon);
+                final LatLng user = new LatLng(lat, lon);
+        /*used marker for show the location */
+                Marker hamburg = map.addMarker(new MarkerOptions()
+                        .position(user)
+                        .title(editText7.getText().toString())
+                );
+                // Move the camera instantly to hamburg with a zoom of 15.
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(user, 15));
+
+                // Zoom in, animating the camera.
+                map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 
             }
-            if(addresses.size()>0)
-            {
-                Address address=addresses.get(0);
-                LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
-                map.addMarker(new MarkerOptions().position(latLng).title("Temp"));
-                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            }
         } catch (IOException e) {
+
             e.printStackTrace();
         }
+
+    }
+
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+
+                if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+
+                {
+
+                    if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) !=  PackageManager.PERMISSION_GRANTED)
+
+                    {
+
+
+
+                        map.setMyLocationEnabled(true);
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    Toast.makeText(this,"Permission Denied" , Toast.LENGTH_LONG).show();
+
+                }
+
+
+
+    }
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
+
+    {
+
+
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+
+        googlePlaceUrl.append("location="+latitude+","+longitude);
+
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+
+        googlePlaceUrl.append("&type="+nearbyPlace);
+
+        googlePlaceUrl.append("&sensor=true");
+
+        googlePlaceUrl.append("&key="+"AIzaSyBLEPBRfw7sMb73Mr88L91Jqh3tuE4mKsE");
+
+        Toast.makeText(this, googlePlaceUrl, Toast.LENGTH_SHORT).show();
+
+        Log.d("MapsActivity", "url = "+googlePlaceUrl.toString());
+
+
+
+        return googlePlaceUrl.toString();
 
     }
 
@@ -178,6 +275,8 @@ public class Index extends AppCompatActivity implements OnMapReadyCallback {
         listView.setAdapter(arrayAdapter);
     }
 
+
+    //search
     void AddEvent() {
         //  myDatabase.db_delete();
         Cursor cursor = database.rawQuery("select * from storeon", null);
@@ -210,6 +309,35 @@ public class Index extends AppCompatActivity implements OnMapReadyCallback {
 
     }
 
+
+   public void restaurent(View view)
+    {
+        Object dataTransfer[] = new Object[2];
+
+        GetNearbyBanksData getNearbyPlacesData = new GetNearbyBanksData();
+
+        map.clear();
+
+        String hospital = "ATM";
+
+
+        String url = getUrl(latitude, longitude, hospital);
+
+        dataTransfer[0] = map;
+
+        dataTransfer[1] = url;
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(userLocation)
+                .radius(1000)
+                .fillColor(0x550000FF).strokeColor(0x550000FF));
+
+
+        getNearbyPlacesData.execute(dataTransfer);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 20));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+    }
 
 
 
